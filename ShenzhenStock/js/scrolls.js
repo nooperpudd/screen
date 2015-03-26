b7cf734236c6900265a4d82e5f7bbd32f579d4d0
winton <365504029@qq.com>
2015-03-26 11:47:29 +0800

    refactor the code.

---
+++ ShenzhenStock/js/scrolls.js
@@ -0,0 +1,18 @@
+var scroll = (function () {
+
+    function init() {
+        var speed = 15;
+        var scroll_begin = document.getElementById("scroll_begin");
+        var scroll_end = document.getElementById("scroll_end");
+        var scroll_div = document.getElementById("scroll_div");
+        scroll_end.innerHTML = scroll_begin.innerHTML;
+        function Marquee() {
+            if (scroll_end.offsetWidth - scroll_div.scrollLeft <= 0)
+                scroll_div.scrollLeft -= scroll_begin.offsetWidth;
+            else
+                scroll_div.scrollLeft++;
+        }
+        setInterval(Marquee, speed);
+    }
+    return {init: init}
+})();
